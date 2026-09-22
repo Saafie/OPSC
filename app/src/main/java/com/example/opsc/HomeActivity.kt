fce8ca8efb4594486a3dc1288e79e5c7cc2f9324
Saafie <ST10446806@rcconnect.edu.za>
2026-09-22 14:27:48 +0200

    The app on the emulator is finally working, manifest ad theme was updated to make it work, changes in designs were made any incorrections as well

--- app/src/main/java/com/example/opsc/HomeActivity.kt
+++ app/src/main/java/com/example/opsc/HomeActivity.kt
@@ -1,17 +1,16 @@
-package com.example.opsc.ui.theme
+package com.example.opsc
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.opsc.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/opsc/MainActivity.kt
+++ app/src/main/java/com/example/opsc/MainActivity.kt
@@ -1,35 +1,66 @@
 package com.example.opsc
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Html
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
 import android.widget.EditText
-import androidx.core.text.HtmlCompat
 
 
 class MainActivity : ComponentActivity() {
+    private lateinit var emailInput: EditText
+    private lateinit var passwordInput: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-    val forgotPassword = findViewById<TextView>(R.id.textView4)
-     forgotPassword.text = HtmlCompat.fromHtml(getString(R.string.forgot_password), HtmlCompat.FROM_HTML_MODE_LEGACY)
-    val passwordInput = findViewById<EditText>(R.id.editTextTextPassword)
+        val forgotPassword = findViewById<TextView>(R.id.textView4)
 
-    forgotPassword.setOnClickListener {
-        Toast.makeText(this, "Forgot Password Clicked", Toast.LENGTH_SHORT).show()
+        val loginButtonNav =findViewById<Button>(R.id.loginbutton)
+         passwordInput = findViewById<EditText>(R.id.editTextTextPassword)
+         emailInput = findViewById<EditText>(R.id.editTextTextEmailAddress)
 
-    }
+        //Ensures the forgot password is underlined
+        val rawText = getString(R.string.forgot_password)
+        val underlineSpannable = SpannableString(rawText).apply{
+            setSpan(UnderlineSpan(),0,length,0)
+        }
+        forgotPassword.text = underlineSpannable
 
+        //when the forgot password is clicked
+        forgotPassword.setOnClickListener {
+            Toast.makeText(this, "Forgot Password Clicked", Toast.LENGTH_SHORT).show()
+
+        }
+
+
+        //Logs in when the login button is pressed
+        loginButtonNav.setOnClickListener {
+            attemptLogin()
+        }
+
+        passwordInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE){
+               attemptLogin()
+                true}
+            else {
+                false
+            }
+        }
+
+        //This is checked if enter has been pressed
         passwordInput.setOnKeyListener { _, keyCode, event ->
             if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER){
-                val enterPassword = passwordInput.text.toString()
-
-                Toast.makeText(this, "Enter pressed", Toast.LENGTH_SHORT).show()
+               attemptLogin()
                 true}
             else {
                 false
@@ -37,4 +68,34 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+
+    private fun attemptLogin(){
+        val email = emailInput.text.toString().trim()
+        val password = passwordInput.text.toString()
+
+        if(email.isEmpty()) {
+            emailInput.error = "Please enter your email"
+            emailInput.requestFocus()
+            return
+        }
+
+        if (password.isEmpty()){
+            passwordInput.error = "Please enter your password"
+            passwordInput.requestFocus()
+            return
+        }
+        //Authenticates it against the Rest API
+        authenticateWithApi(email, password)
+    }
+
+    private fun authenticateWithApi(email: String, password: String){
+        Toast.makeText(this, "Logging in", Toast.LENGTH_SHORT ).show()
+        navigateToHome()
+    }
+
+    private fun navigateToHome() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish() // This will close the main activity
+    }
 }
